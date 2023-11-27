833343aad86d37b47f36df8f5167cf5ad2d9f1ef
Thomas Kottke <t.kottke90@gmail.com>
2023-11-27 14:01:12 -0600

    feat: Add youtube json interfaces

---
+++ server/src/interfaces/youtube.interface.ts
@@ -0,0 +1,78 @@
+export interface Thumbnail {
+  height: number;
+  url: string;
+  width: number;
+  resolution: string;
+  id: string;
+}
+
+export interface Format {
+  asr?: number;
+  filesize?: number;
+  format_id: string;
+  format_note: string;
+  fps?: number;
+  height?: number;
+  quality: number;
+  tbr: number;
+  url: string;
+  width?: number;
+  ext: string;
+  vcodec: string;
+  acodec: string;
+  abr?: number;
+  container?: string;
+  format: string;
+  protocol: string;
+  vbr?: number;
+}
+
+export interface YoutubeDetails {
+  id: string;
+  title: string;
+  formats: Format[];
+  thumbnails: Thumbnail[];
+  description: string;
+  upload_date: string;
+  uploader: string;
+  uploader_id: any;
+  uploader_url: string;
+  channel_id: string;
+  channel_url: string;
+  duration: number;
+  view_count: number;
+  average_rating: any;
+  age_limit: number;
+  webpage_url: string;
+  categories: string[];
+  tags: string[];
+  is_live: any;
+  channel: string;
+  extractor: string;
+  webpage_url_basename: string;
+  extractor_key: string;
+  playlist: any;
+  playlist_index: any;
+  thumbnail: string;
+  display_id: string;
+  requested_subtitles: any;
+  format: string;
+  format_id: string;
+  width: number;
+  height: number;
+  resolution: any;
+  fps: number;
+  vcodec: string;
+  vbr: number;
+  stretched_ratio: any;
+  acodec: string;
+  abr: number;
+  ext: string;
+  fulltitle: string;
+  _filename: string;
+}
+
+export enum YoutubePostMetadataKeys {
+  'SOURCE_ID' = 'SOURCE_ID',
+  'OVER_18' = 'OVER_18'
+}
