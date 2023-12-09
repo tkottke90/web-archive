47041eba7926fd4ff9786578b3fab4709f63262d
Thomas Kottke <t.kottke90@gmail.com>
2023-12-09 10:17:50 -0600

    refactor: Add nfsw query param to youtube parser

--- server/src/controllers/parser.controller.ts
+++ server/src/controllers/parser.controller.ts
@@ -3,12 +3,18 @@ import express from 'express';
 import { Inject } from '@decorators/di';
 import { RedditScraper } from '../services';
 import { YoutubeParser } from '../services/youtube.service';
+import { PostTagDao } from '../dao/post-tag.dao';
+import { PostDao } from '../dao/post.dao';
+import { ZodQueryValidator } from '../middleware/zod.middleware';
+import { z } from 'zod';
 
 @Controller('/parsers')
 export class ParserController {
   constructor(
     @Inject('RedditScraper') private readonly reddit: RedditScraper,
-    @Inject('YoutubeParser') private readonly youtube: YoutubeParser
+    @Inject('YoutubeParser') private readonly youtube: YoutubeParser,
+    @Inject('PostDao') private readonly postDao: PostDao,
+    @Inject('PostTagDao') private readonly postTagDao: PostTagDao
   ) {}
 
   @Get('/reddit')
@@ -26,9 +32,17 @@ export class ParserController {
     }
   }
 
-  @Get('/youtube')
+  @Get('/youtube', [
+    ZodQueryValidator(
+      z.object({
+        target: z.string(),
+        nfsw: z.boolean({ coerce: true }).optional()
+      })
+    )
+  ])
   async getYoutubeVideo(
     @Query('target') target: string,
+    @Query('nfsw') nfsw: boolean,
     @Response() res: express.Response,
     @Next() next: express.NextFunction
   ) {
