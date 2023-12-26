3a90ae4325ce9b319223b85462e694c6e95062f4
Thomas Kottke <t.kottke90@gmail.com>
2023-12-25 21:44:00 -0600

    refactor: Add shadow to drawer

--- ui/src/components/Layouts/DrawerLayout.tsx
+++ ui/src/components/Layouts/DrawerLayout.tsx
@@ -11,7 +11,7 @@ export function DrawerLayout(props: DrawerProps) {
 
   return (
     <main class="h-full w-full grid grid-cols-[250px_1fr]">
-      <aside class="bg-burnt-500 text-cloud-200 h-full flex flex-col gap-1">
+      <aside class="bg-burnt-500 text-cloud-200 h-full flex flex-col gap-1 shadow-sm ">
         <h2 class="text-4xl text-center my-2">Web Archive</h2>
         <section class="min-h-1 grow">
 
